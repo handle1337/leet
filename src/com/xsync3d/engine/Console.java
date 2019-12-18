96e39c494ea4baaace915d744f662c3053085810
Andres Martinez <mewsiof@gmail.com>
2019-12-17 23:31:46 -0700

    Got rid of unused package, added basic functions to the Console class.

---
+++ src/com/xsync3d/engine/Console.java
@@ -0,0 +1,14 @@
+package com.xsync3d.engine;
+
+public class Console {
+
+    public void loop() {
+        while (true) {
+
+        }
+    }
+
+    public void run(String[] args) {
+
+    }
+}
--- src/com/xsync3d/engine/console.java
+++
@@ -1,5 +0,0 @@
-package com.xsync3d.engine;
-
-public class console {
-
-}
